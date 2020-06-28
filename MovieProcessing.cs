49c975c7217c117932876e39bc78b62d4f325506
Hellong2 <51761571+Hellong2@users.noreply.github.com>
2020-06-28 23:54:11 +0200

    Viewing

--- MovieProcessing.cs
+++ MovieProcessing.cs
@@ -7,20 +7,7 @@ namespace Projekt_TMDB
     public static class MovieProcessing
     /// Wszystkie metody zwracające wartości mają znaleźc się tu
     {
-        public static DateTime YearToDatetimeStart (string Year)
-        {
 
-            try
-            {
-                DateTime inputtedYear = new DateTime(int.Parse(Year), 01, 01);
-                return inputtedYear;
-            }
-            catch
-            {
-                Console.WriteLine("To nie jest Rok!");
-            }
-                            
-        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,23 +7,32 @@ namespace Projekt_TMDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(@"Witaj w becie programu przetwarzającego Bazę filmów TMDB. Możesz:
-                                -Wyświetlić najlepsze filmy z wybranego przez ciebie okresu,
-                                -Wyświetlić najbardziej dochodowe filmy z wybranego przez siebie okresu,
-                                -Wyświetlić Producentów, którzy zarobili najwięcej ww wybranym przez ciebie okresie");
+            Console.WriteLine("Witaj w becie programu przetwarzającego Bazę filmów TMDB. Możesz:");
+            Console.WriteLine("- Wyświetlić najlepsze filmy z wybranego przez ciebie okresu,");
+            Console.WriteLine("-Wyświetlić najbardziej dochodowe filmy z wybranego przez siebie okresu,");
+            Console.WriteLine("-Wyświetlić Producentów, którzy zarobili najwięcej w wybranym przez ciebie okresie");
 
             bool check = false;
             Console.WriteLine("Wpisz: Najlepsze filmy/Dochodowe filmy/Producenci: \n");
             string answer = Console.ReadLine();
-            Console.WriteLine(@"Wybierz okres jaki cię interesuje
-                                    -może to być rok lub lata (xxxx, xxxx)");
+            Console.WriteLine("Wybierz okres jaki cię interesuje");
+            Console.WriteLine("-może to być rok lub lata (xxxx), zostaw pusty drugi rok, jeśli chcesz dane z jednego roku");
             string data1 = Console.ReadLine();
+            DateTime startYear1 = new DateTime(int.Parse(data1), 01, 01);
+            DateTime endYear1 = new DateTime(int.Parse(data1), 12, 31);
 
+            //Console.WriteLine(startYear1 + " + " +  endYear1);
 
-            DateTime data2 = DateTime.Parse(Console.ReadLine());
+            string data2 = Console.ReadLine();
+            if (data2 != null)
+            {
+                DateTime startYear2 = new DateTime(int.Parse(data2), 01, 01);
+                DateTime endYear2 = new DateTime(int.Parse(data2), 12, 31);
+            }
+
 
             Console.WriteLine("Wybierz, ile wyników chciałbyś wyświetlić (domyślnie 10): \n");
-                //int quantity = TryParse()
+            int ammount = int.Parse(Console.ReadLine());
             do
             {
 
