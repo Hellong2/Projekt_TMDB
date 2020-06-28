471a05db87b87e70b67d0b0015ba4f6aa8dfae39
Hellong2 <51761571+Hellong2@users.noreply.github.com>
2020-06-28 09:14:56 +0200

    Choosing option added

--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace Projekt_TMDB
     {
         static void Main(string[] args)
         {
-            _ = MoviesList.ListaFilmow;
+            Console.WriteLine(@"Witaj w becie programu przetwarzającego Bazę filmów TMDB. Możesz:
+                                -Wyświetlić najlepsze filmy z wybranego przez ciebie okresu,
+                                -Wyświetlić najbardziej dochodowe filmy z wybranego przez siebie okresu,
+                                -Wyświetlić Producentów, którzy zarobili najwięcej ww wybranym przez ciebie okresie");
+
+            bool check = false;
+            do
+            {
+                Console.WriteLine("Wpisz: Najlepsze filmy/Dochodowe filmy/Producenci");
+                string answer = Console.ReadLine();
+
+                switch (answer)
+                {
+                    case "Najlepsze filmy":
+                        break;
+                    case "Dochodowe filmy":
+                        break;
+                    case "Producenci":
+                        break;
+                    default:
+                        Console.WriteLine("Nie wybrałeś odpowiedniej komendy, spróbuj jeszcze raz");
+                        check = true;
+                        break;
+                }
+            } while (check == true);
+
 
 
             List<Movie> DzienPremiery = MoviesList.MovieDate(new DateTime(2011, 05, 14));
@@ -17,12 +42,12 @@ namespace Projekt_TMDB
                 Console.WriteLine(m.Title);
             }
 
-            List<Movie> Producent = MoviesList.Producer("Weinstein Company, The");
+            //List<Movie> Producent = MoviesList.Producer("Weinstein Company, The");
 
-            foreach (Movie m in Producent)
-            {
-                Console.WriteLine(m.Title);
-            }
+            //foreach (Movie m in Producent)
+            //{
+            //    Console.WriteLine(m.Title);
+            //}
 
 
             Console.ReadKey();
